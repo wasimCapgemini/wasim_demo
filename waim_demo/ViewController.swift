f19d5f4cd24815bee5781a8a32b12572c5e144d7
Wasim Akram <wasim.b.akram@capgemini.com>
2022-07-22 11:08:44 +0530

    Initial Commit

---
+++ waim_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  waim_demo
+//
+//  Created by Wasim Akram on 7/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ waim_demoUITests/waim_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  waim_demoUITestsLaunchTests.swift
+//  waim_demoUITests
+//
+//  Created by Wasim Akram on 7/22/22.
+//
+
+import XCTest
+
+class waim_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
